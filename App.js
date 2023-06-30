9aa6c683d98ac0f91cfd525a0477e23c85c2e235
andremugabo <andremugabo@yahoo.fr>
2023-07-01 00:53:41 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Animated, { SlideInDown, SlideInUp } from "react-native-reanimated";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const AboutScreen = ({ navigation }) => {
+  return (
+    <Animated.View style={styles.container} entering={SlideInUp}>
+      <Text style={styles.text}>About Screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Main")}>
+        <Text>Home</Text>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
+
+const MainScreen = ({ navigation }) => {
+  return (
+    <Animated.View style={styles.container} entering={SlideInUp}>
+      <Text style={styles.text}>Main Screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("About")}>
+        <Text>About</Text>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
   },
 });
